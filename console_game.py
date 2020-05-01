659b91d41d29d1667fd7303727dbdede3585ac03
Msubboti <62071377+Msubboti@users.noreply.github.com>
2020-05-01 12:47:34 +0300

    renaming file Console_game.py

--- console_game.py
+++ console_game.py
@@ -4,12 +4,12 @@ from models import Enemy, Player, Score
 from exceptions import EnemyDown, GameOver
 
 
-
 def play(main_hero):
     """
-
-    :param main_hero:
-    :return:
+    The instruction of game is described in file README.md
+    :param main_hero: account which is created for game and
+    writing result to score table (input is instance of Player class )
+    :return: Nothing. Game will have continued till exception will called.
     """
     start = input('Are you ready to start?Type start if YES\t')
     while start.lower() != "start":
@@ -17,22 +17,22 @@ def play(main_hero):
     print("\n\n************************************************************\n")
     level = 1
     opponent = Enemy(level)
-    round = 1
+    round_of_battle = 1
     while True:
-        print("LEVEL:\t{}\tRound:\t{}. FIGHT".format(level, round))
+        print("LEVEL:\t{}\tRound:\t{}. FIGHT".format(level, round_of_battle))
         try:
             main_hero.attack(opponent)
         except EnemyDown:
             print("Your enemy is dead.")
             level += 1
             main_hero.score += 5
-            round = 1
+            round_of_battle = 1
             opponent = Enemy(level)
             print("You are scored {} points.".format(main_hero.print_scores()))
         else:
             print("You need to get ready for defence.")
             print("Your lives:\t{} || Enemy lives:\t{}".format(main_hero.lives, opponent.lives))
-            round = main_hero.defence(opponent, round)
+            round_of_battle = main_hero.defence(opponent, round_of_battle)
 
 
 
